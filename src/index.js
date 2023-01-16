ed97b89ae1bc416660ec7022ff1477faeb9bdd48
chinchopp <pralgulalex@gmail.com>
2023-01-16 22:38:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    var number = Math.abs(n)
+    var string = number.toString()
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray
 }
